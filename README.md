af29cd640fa9317d963729caa985d4ace2446133
E-B-maxi <emil.becker@gym-alfter.de>
2026-06-19 07:13:17 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Diese Seite bei [https://e-b-maxi.github.io/binaer_to_ascii_callipe_v3_und_lcd1602_i2c/](https://e-b-maxi.github.io/binaer_to_ascii_callipe_v3_und_lcd1602_i2c/) öffnen
+
+## Als Erweiterung verwenden
+
+Dieses Repository kann als **Erweiterung** in MakeCode hinzugefügt werden.
+
+* öffne [https://makecode.calliope.cc/](https://makecode.calliope.cc/)
+* klicke auf **Neues Projekt**
+* klicke auf **Erweiterungen** unter dem Zahnrad-Menü
+* nach **https://github.com/e-b-maxi/binaer_to_ascii_callipe_v3_und_lcd1602_i2c** suchen und importieren
+
+## Dieses Projekt bearbeiten
+
+Um dieses Repository in MakeCode zu bearbeiten.
+
+* öffne [https://makecode.calliope.cc/](https://makecode.calliope.cc/)
+* klicke auf **Importieren** und dann auf **Importiere URL**
+* füge **https://github.com/e-b-maxi/binaer_to_ascii_callipe_v3_und_lcd1602_i2c** ein und klicke auf Importieren
+
+#### Metadaten (verwendet für Suche, Rendering)
+
+* for PXT/calliopemini
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,100 @@
+enum RadioMessage {
+    message2 = 1435,
+    message4 = 29926,
+    message3 = 31126,
+    message1 = 49434
+}
+function EntpackBinaerString() {
+    if (binaerString.length >= 8) {
+        let dezimalZahl = parseInt(binaerString, 2)
+        // Wandelt die Dezimalzahl in das ASCII-Zeichen um
+        asciiZeichen = String.fromCharCode(dezimalZahl)
+        basic.showString(asciiZeichen, 150)
+        // Setzt den String für die nächste Eingabe zurück
+        binaerString = ""
+        I2C_LCD1602.ShowString(asciiZeichen, 0, 0)
+        music.play(music.tonePlayable(Note.B5, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        
+        
+        
+        
+        if (position_outputus == 32) {
+           
+            
+            
+            basic.showLeds(`
+                # . # . #
+                . . # . .
+                . . # . .
+                . . . . .
+                # . # . #
+                `)
+        }
+        position_outputus += 1
+    } else {
+        position_outputus = 1
+         
+        
+        
+        I2C_LCD1602.clear()
+        basic.showLeds(`
+            # . # . #
+            . . # . .
+            . . # . .
+            . . . . .
+            # . # . #
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+}
+// Wenn Knopf A gedrückt wird, füge eine '0' hinzu
+input.onButtonPressed(Button.A, function () {
+    binaerString = "" + binaerString + "0"
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . .
+        # # . . .
+        `)
+        music.play(music.tonePlayable(Note.C, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        
+})
+// Wenn A+B gedrückt wird, wandle die Binärkette in ASCII um
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString(binaerString)
+    EntpackBinaerString()
+    asciiZeichen = ""
+})
+radio.onReceivedString(function (receivedString) {
+    binaerString = receivedString
+    EntpackBinaerString()
+})
+// Wenn Knopf B gedrückt wird, füge eine '1' hinzu
+input.onButtonPressed(Button.B, function () {
+        
+    binaerString = "" + binaerString + "1"
+    
+    basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+    music.play(music.tonePlayable(Note.D, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+       
+})
+let position_outputus = 0
+let binaerString = ""
+let asciiZeichen = ""
+radio.setGroup(815)
+position_outputus = 1
+I2C_LCD1602.LcdInit(39)
